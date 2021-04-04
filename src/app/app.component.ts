549d2b38251a1e30ae233562eb6e7a8318ef319c
ShellsOfSilver <shellsofsilver13@gmail.com>
2021-04-04 16:38:32 +0300

    14 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,45 +21,45 @@ export class AppComponent implements OnInit {
       stepper.next();
       setTimeout(() => {
         this.dataService.setImportanceRandom();
-        this.dataService.setExpertMatrix();
-        stepper.next();
-        setTimeout(() => {
-          this.dataService.setMatrixRandom();
-          this.dataService.setAggregationMatrix();
-          stepper.next();
-          setTimeout(() => {
-            this.dataService.setTriangularFuzzyNumbers();
-            stepper.next();
-            setTimeout(() => {
-              this.dataService.setFuzzyNumbersTransformedLinguisticTerms();
-              stepper.next();
-              setTimeout(() => {
-                this.dataService.setMatrixOptimalValuesCriteria();
-                stepper.next();
-                setTimeout(() => {
-                  this.dataService.setNotmalisedFuzzyMatrix();
-                  stepper.next();
-                  setTimeout(() => {
-                    this.dataService.setNormalizedWMatrix();
-                    stepper.next();
-                    setTimeout(() => {
-                      this.dataService.setoOerallIntervalValuedFuzzyPerformanceRating();
-                      stepper.next();
-                      setTimeout(() => {
-                        this.dataService.setDefuzzification();
-                        stepper.next();
-                        setTimeout(() => {
-                          this.dataService.setDegreeOfUtility();
-                          stepper.next();
-                        });
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
+        // this.dataService.setExpertMatrix();
+        // stepper.next();
+        // setTimeout(() => {
+        //   this.dataService.setMatrixRandom();
+        //   this.dataService.setAggregationMatrix();
+        //   stepper.next();
+        //   setTimeout(() => {
+        //     this.dataService.setTriangularFuzzyNumbers();
+        //     stepper.next();
+        //     setTimeout(() => {
+        //       this.dataService.setFuzzyNumbersTransformedLinguisticTerms();
+        //       stepper.next();
+        //       setTimeout(() => {
+        //         this.dataService.setMatrixOptimalValuesCriteria();
+        //         stepper.next();
+        //         setTimeout(() => {
+        //           this.dataService.setNotmalisedFuzzyMatrix();
+        //           stepper.next();
+        //           setTimeout(() => {
+        //             this.dataService.setNormalizedWMatrix();
+        //             stepper.next();
+        //             setTimeout(() => {
+        //               this.dataService.setoOerallIntervalValuedFuzzyPerformanceRating();
+        //               stepper.next();
+        //               setTimeout(() => {
+        //                 this.dataService.setDefuzzification();
+        //                 stepper.next();
+        //                 setTimeout(() => {
+        //                   this.dataService.setDegreeOfUtility();
+        //                   stepper.next();
+        //                 });
+        //               });
+        //             });
+        //           });
+        //         });
+        //       });
+        //     });
+        //   });
+        // });
       });
     });
   }
